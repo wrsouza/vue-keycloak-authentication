bd653f2609ee6095d8a57167ed1d6150aa986de3
Willian Souza <willian.souza@zooxsmart.com>
2022-02-03 17:29:22 -0300

    feat: get user profile

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,8 +2,49 @@
   <div class="about">
     <h1>This page is authenticated</h1>
     <p>
-    <router-link to="/logout">[ Sair da Sessão ]</router-link>
+      <strong>Id:</strong> <span style="color: blue">{{ user.id }}</span><br>
+      <strong>FirstName:</strong> <span style="color: blue">{{ user.firstName }}</span><br>
+      <strong>LastName:</strong> <span style="color: blue">{{ user.lastName }}</span><br>
+      <strong>E-mail:</strong> <span style="color: blue">{{ user.email }}</span><br>
+      <strong>Username:</strong> <span style="color: blue">{{ user.username }}</span>
+    </p>
+    <p>
+      <router-link to="/logout">[ Sair da Sessão ]</router-link>
     </p>
   </div>
 </template>
 
+<script lang="ts">
+import { defineComponent, reactive } from 'vue'
+import { keycloak } from '../services'
+
+export default defineComponent({
+  setup() {
+    const user = reactive({
+      id: '',
+      firstName: '',
+      lastName: '',
+      email: '',
+      username: ''
+    })
+    return {
+      user
+    }
+  },
+  methods: {
+    async getUserInfo() {
+      const info = await keycloak.loadUserProfile()
+      Object.assign(this.user, {
+        id: info.id,
+        firstName: info.firstName,
+        lastName: info.lastName,
+        email: info.email,
+        username: info.username
+      })
+    }
+  },
+  async mounted() {
+    await this.getUserInfo()
+  }
+})
+</script>
